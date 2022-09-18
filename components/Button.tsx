865869b0579ab736c3d0bb945d84a51dd18deaa4
Robert Chen <robertchen@mac.local>
2022-09-18 01:51:17 -0700

    components folder and init button component

---
+++ components/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const StyledButton = styled.div`
+  /* test */
+`;
+
+const Button = () => {
+  return <StyledButton>Button</StyledButton>;
+};
+
+export default Button;
