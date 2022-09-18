363d58c799652679e84b5dc74f8dd8c138cf27e1
Robert Chen <robertchen@mac.local>
2022-09-18 02:18:22 -0700

    Styles configuration and variables

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const StyledButton = styled.div`
+const StyledButton = styled.button`
   /* test */
+  border: none;
+  border-radius: var(--br);
+  background-color: var(--blue);
+  padding: calc(1.5 * var(--sm)) var(--md);
+  color: var(--white);
+  font-weight: 700;
+  font-size: var(--lg);
 `;
 
-const Button = () => {
-  return <StyledButton>Button</StyledButton>;
+const Button = ({ text }: { text: string }) => {
+  return <StyledButton>{text}</StyledButton>;
 };
 
 export default Button;
