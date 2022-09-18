f4eda23d64aa24001e09bd3c3bf3dfa685d5e4e6
Robert Chen <robertchen@mac.local>
2022-09-18 03:06:31 -0700

    Added components preview page and Button component

---
+++ pages/components.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "../components/Button";
+import styled from "@emotion/styled";
+
+const Table = styled.div`
+  padding: 2em;
+`;
+
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  align-content: space-around;
+  align-items: center;
+  :not(:last-child) {
+    padding-bottom: 2em;
+  }
+`;
+
+const Components = () => {
+  return (
+    <Table>
+      <Row>
+        <Button size="sm" text="Small Button" />
+        <Button link="https://google.com" text="Button with link" />
+        <Button text="Default Button" />
+        <Button size="lg" text="Large Button" />
+      </Row>
+      <Row>Check the usage of these components in pages/components.tsx</Row>
+    </Table>
+  );
+};
+
+export default Components;
