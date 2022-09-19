b179c7a0e342fa855d662335061373f100253731
biryaniq <49655956+biryaniq@users.noreply.github.com>
2022-09-19 01:40:31 -0700

    add NavItem

---
+++ components/NavItem.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Link from "next/link";
+
+const FlexContainer = styled.div`
+  display: flex;
+  align-content: center;
+`;
+
+const NavItem = (NavigationLink) => {
+  <Link href={}>
+    <FlexContainer>
+    
+    
+    </FlexContainer>
+  </Link>
+}
+
+export default NavItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+//components
+import NavItem from '../components/NavItem';
+
 //styled components
 const FlexContainer = styled.div`
   display: flex;
