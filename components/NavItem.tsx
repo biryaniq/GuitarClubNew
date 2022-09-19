c8fdc5d33ca3ccfc94d31054f4877dfb6e441166
biryaniq <49655956+biryaniq@users.noreply.github.com>
2022-09-19 01:59:26 -0700

    add link to navItem

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import styled from "@emotion/styled";
 import Link from "next/link";
+import { NavigationLink } from "./NavBar";
 
 const FlexContainer = styled.div`
   display: flex;
   align-content: center;
 `;
 
-const NavItem = (NavigationLink) => {
-  <Link href={}>
+const NavItem = ({navLink}:{navLink: NavigationLink}) => {
+  <Link href={navLink.path}>
     <FlexContainer>
-    
-    
+    {navLink.name}
     </FlexContainer>
   </Link>
 }
